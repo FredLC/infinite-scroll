a674293e2938eef13f21f00beeb30b770b1eef92
fredlc <fredlefevre@outlook.com>
2020-06-27 16:30:17 -0400

    Implemented infinite scroll

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+let imagesToLoad = 5;
 const apiKey = config.apiKey;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imagesToLoad}`;
+
+function loadImage() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        imagesToLoad = 30;
+    }
+}
 
 function setAttributes(element, attributes) {
     for (const key in attributes) {
@@ -14,23 +26,21 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
         });
         const image = document.createElement('img');
-        // image.setAttribute('src', photo.urls.regular);
-        // image.setAttribute('alt', photo.alt_description);
-        // image.setAttribute('title', photo.alt_description);
         setAttributes(image, {
             src: photo.urls.regular,
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        image.addEventListener('load', loadImage);
         item.appendChild(image);
         imageContainer.appendChild(item);
     });
@@ -45,5 +55,16 @@ async function getPhotos() {
         console.log(error);
     }
 }
+
+window.addEventListener('scroll', () => {
+    if (
+        window.innerHeight + window.scrollY >=
+            document.body.offsetHeight - 1000 &&
+        ready
+    ) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 getPhotos();
